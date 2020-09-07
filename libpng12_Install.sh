743b1e7a4c22319874cdedbee196aeb56bf4af2b
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-09-07 17:59:55 -0400

    Create libpng12_Install.sh

---
+++ libpng12_Install.sh
@@ -0,0 +1,5 @@
+wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+dpkg -i /tmp/libpng12.deb
+rm /tmp/libpng12.deb
+
+*From: https://github.com/tcoopman/image-webpack-loader/issues/95
