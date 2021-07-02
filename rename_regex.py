6b211ad7f7e033aa14ce80f9aaf854922e18d2e6
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2021-07-01 21:15:07 -0400

    Update rename_regex.py

--- rename_regex.py
+++ rename_regex.py
@@ -1,16 +1,15 @@
 import glob, re, os
 
-path = r"D:\Downloads\new\new"
+#path = r"D:\Downloads\new\new"
+path = os.getcwd()
 
 for filename in glob.glob( path + '\\*' ):
 	pattern = r'\.'
 	replace = r' '
 	spaces_name = re.sub(pattern, replace, filename)
 
-	pattern = r'(.*) (19|20)(\d{2}).* (.{3})$'				# movie
-	replace = r'\1 (\2\3).\4'								# movie
-	#pattern = r'(.*) [sS](\d\d)[eE](\d\d).* (.{3})' 		# tv show
-	#replace = r'\1 [\2x\3].\4'								# tv show
+	pattern = r'(\d{3})_(\d{2})\.(\d{2})\.(\d{2})\.mrc' 	
+	replace = r'$1_$2_$3_$4.mrc'				
 	new_name = re.sub(pattern, replace, spaces_name)
 	print(filename + ' -> ' + new_name)
 	os.rename(filename, new_name)
